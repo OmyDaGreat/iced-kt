9e1b101b83376f2fedabd93c5b5db394150efa7f
Om Gupta <ogupta4242@gmail.com>
2025-10-08 21:21:32 -0700

    feat(project): init

---
+++ library/build.gradle.kts
@@ -0,0 +1,136 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+val user: String by project
+val dev: String by project
+val mail: String by project
+val devURL: String by project
+val repo: String by project
+val g: String by project
+val artifact: String by project
+val v: String by project
+val desc: String by project
+val inception: String by project
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.kotlinter)
+}
+
+group = g
+version = v
+
+@OptIn(ExperimentalWasmDsl::class)
+kotlin {
+    jvm()
+
+    androidTarget {
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    js {
+        browser {
+            testTask {
+                useKarma {
+                    useChromiumHeadless()
+                }
+            }
+        }
+        nodejs()
+    }
+    wasmJs {
+        browser {
+            testTask {
+                useKarma {
+                    useChromiumHeadless()
+                }
+            }
+        }
+        nodejs()
+    }
+
+    applyDefaultHierarchyTemplate()
+
+    @Suppress("unused")
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kermit)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "$g.$artifact"
+    compileSdk =
+        libs.versions.android.compileSdk
+            .get()
+            .toInt()
+    defaultConfig {
+        minSdk =
+            libs.versions.android.minSdk
+                .get()
+                .toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(g, artifact, v)
+
+    pom {
+        name = repo
+        description = desc
+        inceptionYear = inception
+        url = "https://github.com/$user/$repo"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://mit.malefic.xyz"
+            }
+        }
+        developers {
+            developer {
+                name = dev
+                email = mail
+                url = devURL
+            }
+        }
+        scm {
+            url = "https://github.com/$user/$repo"
+            connection = "scm:git:git://github.com/$user/$repo.git"
+            developerConnection = "scm:git:ssh://github.com/$user/$repo.git"
+        }
+    }
+}
+
+dokka {
+    pluginsConfiguration.html {
+        footerMessage.set("&copy; 2025 $dev <$mail>")
+    }
+}
